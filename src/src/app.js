6f8bac3090fbeb9a100ba2824080b733ff226ea9
Habiba Khaled <48651088+HabibaKhaledMohammed@users.noreply.github.com>
2021-01-24 17:31:55 +0200

    Monster Slayer

---
+++ src/src/app.js
@@ -0,0 +1,74 @@
+new Vue({
+  el: "#app",
+  data: {
+    actionLog: [],
+    monsterHealth: 100,
+    playerHealth: 100,
+    showNewGameControls: true,
+    healCount: 3,
+    attackCount: 0,
+    damageDealt: 0,
+    damageTaken: 0,
+    hmSpecialAttacks: 0,
+  },
+  methods: {
+    rand(min, max) {
+      return Math.floor(Math.random() * ((max - min) + 1) + min);
+    },
+    increaseAttackCount() {
+      if (this.attackCount < 8) {
+        this.attackCount++;
+      }
+    },
+    playerAttack() {
+     
+    },
+    playerSpecialAttack() {
+    
+    },
+    monsterAttack(min, max) {
+     
+    },
+    healPlayer() {
+     
+    },
+    startNewGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.healCount = 3;
+      this.attackCount = 0;
+      this.damageDealt = 0;
+      this.damageTaken = 0;
+      this.hmSpecialAttacks= 0;
+      this.showNewGameControls = false;
+      this.actionLog = [];
+    },
+    playerSurrender() {
+      this.playerHealth = 0;
+      this.showNewGameControls = true;
+    },
+    logActioin(actionTaken, entity, color, value) {
+      let action = {
+        actionTaken: actionTaken,
+        entity: entity,
+        class: 'alert-' + color,
+        actionValue: value
+      };
+      this.actionLog.push(action);
+    },
+  },
+  computed: {
+    healsUsed: function() {
+     
+    }
+  },
+  watch: {
+    monsterHealth: function() {
+      
+    }
+    ,
+    playerHealth: function() {
+     
+    }
+  },
+})
